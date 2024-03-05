9ac6964d54eef59c5a8494ba8bf0c59c9afd31a5
gotrim <147737827+gotrim@users.noreply.github.com>
2024-03-05 17:57:54 -0300

    05/03/24

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("searchForm").addEventListener("submit", function(event) {
 
         var searchValue = document.getElementById("search").value; // Pega o valor do input de pesquisa
@@ -7,4 +7,33 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("NATHAM GAY");
         }
     });
-});
+})*/
+
+const content = document.querySelector(".content");
+const search = document.querySelector("input[type='search']");
+
+let items = [];
+
+inputSearch.oninput = () => {
+    content.innerHTML = "";
+
+    items
+        .filter((item) => {
+            item.toLowerCase(.includes(inputSearch))
+        })
+}
+
+function addHTML(item) {
+    const div = document.createElement("div");
+    div.innerHTML = item;
+    content.append(div);
+}
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then((data) => data.json())
+    .then((users)=> {
+        users.forEach((user)=> {
+            addHTML(user.name);
+            items.push(user.name);
+        });
+    });
