263e71856430cf1c5d05a16638ef75f6ffb3f7dc
Arthur <arthur.36.fbatista@gmail.com>
2024-03-15 16:08:44 -0300

    Update banco.js

--- src/banco.js
+++ src/banco.js
@@ -5,8 +5,13 @@ const sequelize = new Sequelize('db_teste', 'root', 'root', {
     port: 3307
 })
 
+const app = express();
+
 sequelize.authenticate().then(function(){
     console.log("Conectado com sucesso!")
 }).catch(function(erro){
     console.log("Falha ao se conectar: "+erro)
 })
+
+app.post('add-')
+
