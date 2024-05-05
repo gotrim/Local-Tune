c5ed7cbc4fee51e65fc9967c4edf8f4c86d84727
gotrim <147737827+gotrim@users.noreply.github.com>
2024-05-05 07:18:51 -0300

    na moral

--- style/estilojs/script.js
+++ style/estilojs/script.js
@@ -3,7 +3,7 @@ function barra() {
     menu.classList.toggle('active');
 
     if (menu.classList.contains('active')) {
-        document.getElementById("esquerda").style.width = "calc(60% - 100px)";
+        document.getElementById("esquerda").style.width = "calc(40% - 100px)";
         document.getElementById("conteudo-barra").style.display = "block";
     } else {
         document.getElementById("esquerda").style.width = "60px";
